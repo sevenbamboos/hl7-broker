86028261aa0ecbaea7cc2dd846f5300f6775be50
sevenbamboos <sevenbamboos@gmail.com>
2017-03-19 10:57:54 +0800

    add mllp tokenizer

--- src/hl7-server.app.ts
+++ src/hl7-server.app.ts
@@ -1,4 +1,4 @@
 import * as hl7Server from './hl7-server';
 
-const demoServer = new hl7Server.HL7Server(new hl7Server.RawByteTokenizer(), 'utf8');
+const demoServer = new hl7Server.HL7Server('utf8');
 demoServer.listen(4000);
--- src/hl7-server.ts
+++ src/hl7-server.ts
@@ -1,39 +1,78 @@
+///<reference path="../node_modules/@types/node/index.d.ts" />
 const net = require('net');
+const EventEmitter = require('events').EventEmitter;
 
-export interface MessageTokenizer {
-  feed(data: Buffer): void;
-  next(): string;
-}
+export const mllpStartBlock: number = 0x0B;
+export const mllpEndBlock: number = 0x1C;
+export const carriageReturn: number = 0x0D;
 
-export class RawByteTokenizer implements MessageTokenizer {
+class MllpTokenizer {
 
-  feed(data: Buffer): void {
-    //TODO
-    console.log(data);
+  private contents: string = '';
+  private lastByte: number;
+  private eventEmitter: any = new EventEmitter();
+
+  onData(byte: number) {
+    if (byte === mllpStartBlock) {
+      this.onStartBlock();
+    } else if (byte === mllpEndBlock) {
+      this.onEndBlock();
+    } else if (byte === carriageReturn) {
+      this.onCarriageReturn();
+    } else {
+      this.onContents(byte);
+    }
   }
-  next(): string {
-    throw new Error('Method not implemented.');
+
+  onClose() {
+    console.log(`receive: ${this.contents}`);
+  }
+
+  private onStartBlock() {
+    this.contents = '';
+  }
+
+  private onEndBlock() {
+    this.lastByte = mllpEndBlock;
+  }
+
+  private onCarriageReturn() {
+    if (this.lastByte === mllpEndBlock) {
+      this.eventEmitter.emit('message', this.contents);
+    } else {
+      this.onContents(carriageReturn);
+    }
+  }
+
+  private onContents(byte: number) {
+    this.contents.concat(String.fromCharCode(byte));
   }
 
 }
 
 export class HL7Server {
 
+  private messageTokenizer: MllpTokenizer = new MllpTokenizer();
+
   constructor(
-    private msgTokenizer: MessageTokenizer,
     private encoding: string
     ) {
   }
 
   listen(port: number) {
-    const server = net.createServer((socket: any) => {
-      socket.setEncoding(this.encoding);
+    const server = net.createServer((connection: any) => {
+      //connection.setEncoding(this.encoding);
 
-      socket.on('data', (data: Buffer) => {
-        this.msgTokenizer.feed(data);
+      connection.on('data', (data: Buffer) => {
+        for (const byte of data.values()) {
+          this.messageTokenizer.onData(byte);
+        }
       });
 
-      socket.end('goodbye\n');
+      connection.on('close', () => {
+        this.messageTokenizer.onClose();
+      });
+
     }).on('error', (err: any) => {
       // handle errors here
       throw err;
