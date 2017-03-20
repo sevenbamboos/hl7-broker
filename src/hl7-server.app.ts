30c56a6f0a065e06d17d8b1c6cf34a282716eba6
sevenbamboos <sevenbamboos@gmail.com>
2017-03-20 22:10:14 +0800

    add dependency to rxjs

--- src/hl7-server.app.ts
+++ src/hl7-server.app.ts
@@ -1,4 +1,12 @@
 import * as hl7Server from './hl7-server';
+import { Observable } from "rxjs/Rx";
+
+Observable.of(1,2,3)
+  .subscribe(
+    (data:any) => console.log(`${data}`),
+    (error:any) => console.error(`error:${error}`),
+    () => console.log('Completed')
+  ); 
 
 const demoServer = new hl7Server.HL7Server('utf8');
 demoServer.listen(4000);
