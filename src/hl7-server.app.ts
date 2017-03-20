cac5233601430010762b656cf5c7ad0650ff2fcd
sevenbamboos <sevenbamboos@gmail.com>
2017-03-20 22:26:19 +0800

    add rxjs test

--- src/hl7-server.app.ts
+++ src/hl7-server.app.ts
@@ -1,12 +1,23 @@
 import * as hl7Server from './hl7-server';
 import { Observable } from "rxjs/Rx";
 
-Observable.of(1,2,3)
-  .subscribe(
+const subject = Observable.of(1,2,3);
+
+const subscription1 = subject.subscribe(
     (data:any) => console.log(`${data}`),
     (error:any) => console.error(`error:${error}`),
     () => console.log('Completed')
-  ); 
+); 
+
+subscription1.unsubscribe;
+
+const subscription2 = subject.subscribe(
+    (data:any) => console.log(`sub2:${data}`),
+    (error:any) => console.error(`error from sub2:${error}`),
+    () => console.log('Completed sub2')
+); 
+
+subscription2.unsubscribe;
 
 const demoServer = new hl7Server.HL7Server('utf8');
 demoServer.listen(4000);
