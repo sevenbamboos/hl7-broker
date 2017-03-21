97f7dc94372a4a6f047963986c40f956c269c0e4
Sam Wang <sevenbamboos@gmail.com>
2017-03-21 13:58:35 +0800

    refactor with observable's bufferToggle

--- src/hl7-server.app.ts
+++ src/hl7-server.app.ts
@@ -1,12 +1,4 @@
-import * as hl7Server from './hl7-server';
-import { Observable } from "rxjs/Rx";
+import * as hl7Server from "./hl7-server";
 
-Observable.of(1,2,3)
-  .subscribe(
-    (data:any) => console.log(`${data}`),
-    (error:any) => console.error(`error:${error}`),
-    () => console.log('Completed')
-  ); 
-
-const demoServer = new hl7Server.HL7Server('utf8');
+const demoServer = new hl7Server.HL7Server("utf8");
 demoServer.listen(4000);
